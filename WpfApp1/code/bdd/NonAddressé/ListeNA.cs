1dec5a2d13f9d721ce6a55b5265881533407ca5b
Bonamy antoine <antoine.bonamy@hotmail.fr>
2019-01-02 10:05:55 +0100

    .hvn

---
+++ WpfApp1/code/bdd/NonAddressé/ListeNA.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.code.bdd.NonAddressé
+{
+    class ListeNA
+    {
+        public List<NA> _NAs;
+
+
+        public void ReadCp(string text)
+        {
+            _NAs = new List<NA>();
+            string str = text;
+            str = str.Replace('\r', ' ');
+            string[] vs = str.Split('\n');
+
+            for (int i = 1; i < vs.Length; i++)
+            {
+                try
+                {
+                    string line = vs[i];
+                    string[] item = line.Split('\t');
+                    var na = new NA();
+
+                    na.Lib = item[2];
+                    na.Ean = long.Parse(item[3]);
+                    _NAs.Add(na);
+
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+
+            }
+            foreach (NonAddresseS nonAddresseS in _list)
+            {
+                bdd.AddProduit(nonAddresseS);
+
+                Console.WriteLine("ok");
+            }
+
+
+        }
+
+
+    }
+}
---
+++ WpfApp1/code/bdd/NonAddressé/NA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.code.bdd.NonAddressé
+{
+    public class NA
+    {
+      public  long Ean;
+       public string Lib;
+
+
+    }
+}
